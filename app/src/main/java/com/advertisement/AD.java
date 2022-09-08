698abf433827632f36ae82f32f017c7abd649191
shengdao <211471317@qq.com>
2022-09-08 10:36:56 +0800

    确定总体模块，先解决软件入口的界面问题

---
+++ app/src/main/java/com/advertisement/AD.java
@@ -0,0 +1,8 @@
+package com.advertisement;
+
+/**
+ * 用户登录完成后，播放广告界面，在此时间，将用户的数据库的文件读入内存
+ */
+public class AD {
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import com.login.*;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
