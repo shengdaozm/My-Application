256be189d7340bb201c7962c1fc7a3464ae78e6b
24435 <2443520715@qq.com>
2022-09-09 17:49:04 +0800

    登录界面

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.myapplication;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-import com.SQLite.*;
+import com.SQlite.*;
 import com.example.myapplication.R;
 import com.firstpage.*;
 import com.login.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    //声明控件
+    private Button myBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
