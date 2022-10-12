ac4e63487b4a9cbf56422aefa86e8d261dbd950d
yeiwsp <yeiwsp@sina.com>
2022-10-12 15:55:09 +0800

    欢迎界面（会闪退）

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -22,16 +22,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("TAG","软件启动");
         super.onCreate(savedInstanceState);
-        isFirst isJump;
-        try {
-            isJump = new isFirst();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if (isJump.getIsfirst()) {
-            Intent intent= new Intent(com.example.myapplication.MainActivity.this, webpage.class);
+//        isFirst isJump;
+//        try {
+//            isJump = new isFirst();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        if (isJump.getIsfirst()) {
+//            Intent intent= new Intent(com.example.myapplication.MainActivity.this, webpage.class);
+//            startActivity(intent);
+//        }
+        Intent intent= new Intent(com.example.myapplication.MainActivity.this, webpage.class);
             startActivity(intent);
-        }
+
         setContentView(R.layout.activity_main);
 
         ViewPager viewpaper = (ViewPager) findViewById(R.id.viewpager);
@@ -41,10 +44,19 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater li = getLayoutInflater();
 
         Button mainBtn = (Button) findViewById(R.id.mainBtn);
+
+        view_list.add(li.inflate(R.layout.view_one,null,false));
+        view_list.add(li.inflate(R.layout.view_two,null,false));
+        view_list.add(li.inflate(R.layout.view_three,null,false));
+
+        MsPagerAdapter mAdapter =  new MsPagerAdapter(view_list);
+
+        viewpaper.setAdapter(mAdapter);
     }
 
     public void changeActivity(View view) {
         Intent intent= new Intent(com.example.myapplication.MainActivity.this, com.login.login.class);
         startActivity(intent);
     }
+
 }
---
+++ app/src/main/java/com/example/myapplication/MsPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class MsPagerAdapter extends PagerAdapter {
+
+    private ArrayList<View> mData;
+
+    public MsPagerAdapter() {
+    }
+
+    public MsPagerAdapter(ArrayList<View> view_list) {
+        super();
+        this.mData = view_list;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(mData.get(position));
+        return mData.get(position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(mData.get(position));
+    }
+}
