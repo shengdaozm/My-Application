c29e8fe27dd1c5a4793f2ed7981d5a47b0475a62
24435 <2443520715@qq.com>
2022-11-01 15:10:04 +0800

    tianjia

--- app/src/main/java/com/webpage/collectionShow.java
+++ app/src/main/java/com/webpage/collectionShow.java
@@ -3,15 +3,36 @@ package com.webpage;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
+import com.SQlite.SQLiteMaster;
 import com.example.myapplication.R;
+import com.publicClass.Labol;
+import com.publicClass.history;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 // //https://www.dmoe.cc/random.phpkaolv 使用随机图API
 public class collectionShow extends AppCompatActivity {
+
+    SQLiteMaster mSQLiteMaster;
+    List<Labol> mLabols = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("TEST", "进入收藏的记录页面 !");
         setContentView(R.layout.collectios);
+    }
+
+    public void getFromDB() throws IllegalAccessException, InstantiationException {
+        mSQLiteMaster = new SQLiteMaster(collectionShow.this);
+        mSQLiteMaster.openDataBase();
+        mLabols = mSQLiteMaster.mLabolDBDao.queryDataList();
+        Collections.reverse(mLabols);//反转mHistories 按照时间的新旧进行排序
+    }
+
+    public void onDestory(){
+        mSQLiteMaster.closeDataBase();
     }
 }
